d8c4fd75024c6e0f30c51635144cfbcba3a2ce6c
littlebuffett <76998855+littlebuffett@users.noreply.github.com>
2021-08-07 07:43:41 +0900

    Add files via upload

---
+++ Study/Check Box/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.checkbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CheckBox chk_red,chk_blue,chk_green;
+    private TextView tv_result;
+    private Button btn_result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chk_blue = findViewById(R.id.chk_blue);
+        chk_green = findViewById(R.id.chk_green);
+        chk_red = findViewById(R.id.chk_red);
+        tv_result = findViewById(R.id.tv_result);
+        btn_result = findViewById(R.id.button_result);
+
+        btn_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str_result = ""; //클릭이 될 때마다 초기화 시켜줌.
+                if(chk_red.isChecked()){
+                    str_result += chk_red.getText().toString();
+                }
+                if(chk_blue.isChecked()){
+                    str_result += chk_blue.getText().toString();
+                }
+                if(chk_green.isChecked()){
+                    str_result += chk_green.getText().toString();
+                }
+                tv_result.setText(str_result);
+            }
+        });
+
+    }
+}
