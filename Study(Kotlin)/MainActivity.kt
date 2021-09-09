6bff512394ab15f064df35aad9f87a5fc0cd415f
littlebuffett <76998855+littlebuffett@users.noreply.github.com>
2021-09-09 20:55:03 +0900

    Update MainActivity.kt

--- Study(Kotlin)/MainActivity.kt
+++ Study(Kotlin)/MainActivity.kt
@@ -1,11 +1,11 @@
 package edu.ourincheon.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity //androidx는 확장된 extension 라이브러리임.
+import android.os.Bundle //오래전부터 사용해온 안드로이드 라이브러리
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity() { // 자식 클래스 : 부모클래스 , ()가 있다는 것은 생성잘르 호출한 것 cf)인터페이스 상속
+    override fun onCreate(savedInstanceState: Bundle?) { //fun은 함수를 의미. AppCompatActivity의 onCreate를 재정의(오버라이드) 하겠다는 것.
+        super.onCreate(savedInstanceState) // super, 즉 AppCompatActivity에 있는 onCreate를 호출한 것임.
+        setContentView(R.layout.activity_main) 
     }
 }
