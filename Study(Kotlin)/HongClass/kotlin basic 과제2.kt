f7f0d9b61962b618f5cc7b017ce59d82eb26de8f
병하 강 <76998855+littlebuffett@users.noreply.github.com>
2021-09-29 21:54:22 +0900

    Update kotlin basic 과제2.kt

--- Study(Kotlin)/HongClass/kotlin basic 과제2.kt
+++ Study(Kotlin)/HongClass/kotlin basic 과제2.kt
@@ -155,3 +155,184 @@ fun main() {
     c decreaseBy 15 //c.decreaseBy(15)
     println("${c.left}, ${c.right}")
 }
+
+
+Kotlin-class
+Class
+class Person{
+   var name:String ="Hong" //생성자가 없을 경우 반드시 초기화 해야 한다.
+   var age:Int = 33
+   var isMarried:Boolean = true
+
+   fun getName() = println("the name is $name")
+}
+fun main() {
+    val hong = Person()
+    println(hong.age)
+    hong.getName()
+}
+
+class Person{
+   var name:String
+   var age:Int
+   var isMarried:Boolean
+
+   constructor(name:String, age:Int, isMarried:Boolean){ //보조 생성자
+       this.name = name
+       this.age = age
+       this.isMarried = isMarried
+   }
+   fun getName() = println("the name is $name")
+}
+fun main() {
+    val lee = Person("Lee", 33, false)
+    println(lee.age)
+    lee.getName()
+}
+
+class Person{
+   var name:String
+   var age:Int
+   var isMarried:Boolean
+   constructor(_name:String, _age:Int, _isMarried:Boolean){ //보조 생성자
+       name = _name
+       age = _age
+       isMarried = _isMarried
+   }
+   fun getName() = println("the name is ${this.name}")
+}
+fun main() {
+    val lee = Person("Kim", 22, false)
+    println(lee.age)
+    lee.getName()
+}
+
+class Person{
+   var name:String
+   var age:Int
+   var isMarried:Boolean
+   constructor(_name:String, _age:Int, _isMarried:Boolean){ //보조 생성자
+       name = _name
+       age = _age
+       isMarried = _isMarried
+   }
+    constructor(_name:String, _age:Int){ //파라미터의 수를 다르게 하여 생성자를 여러개 생성할 수 있다.
+        name = _name
+        age = _age
+        isMarried = false
+    }
+    fun getName() = println("the name is ${this.name}")
+}
+fun main() {
+    val kim = Person("Kim", 22, false)
+    val lee = Person("Lee", 24)
+    println(kim.age)
+    kim.getName()
+    lee.getName()
+}
+class Person (var name:String, //마치 함수처럼 멤버 변수를 선언할 수 있다.(기본생성자)
+              var age:Int,
+              var isMarried:Boolean) {
+    fun getName() = println("the name is ${this.name}")
+}
+fun main() {
+    val kim = Person("Hora", 37, false)
+    kim.getName()
+    println("${kim.age}")
+    if (kim.isMarried)
+        println("${kim.name} is already married.")
+    else
+        println("${kim.name} is not marred yet")
+}
+
+
+class Person (var name:String,
+              var age:Int,
+              var isMarried:Boolean) {
+    init {
+        println("Beginning of the init block")
+        println("이름=${this.name}, 나이=$age")
+        println("End of the init block")
+    }
+    fun getName() = println("the name is ${this.name}")
+}
+fun main() {
+    val kim = Person("Hora", 37, false)
+    kim.getName()
+    println("${kim.age}")
+}
+
+
+class Person (var name:String, var age:Int, var isMarried:Boolean) {
+    var nickname:String = ""
+    init {
+        println("이름=${this.name}, 나이=$age")
+    }
+    constructor(_name:String, _age:Int, _isMarried:Boolean,_nickname:String)
+        :this (_name,_age,_isMarried){
+            this.nickname = _nickname
+    }
+    fun getName() = println("the name is ${this.name}")
+}
+fun main() {
+    val kim = Person("Hora", 37, false,"Chic")
+    kim.getName()
+    println("${kim.nickname}")
+}
+
+
+
+class Rectangle ( _shape:String, _height:Int, _width:Int){
+    val shape:String = _shape
+        get() = field
+    var height:Int = _height
+        get() = field
+        set(value){
+            field = value
+        }
+    var width:Int = _width
+        get() = field
+        set(value){
+            field = value
+        }
+}
+fun main() {
+    val rect = Rectangle("rectangle",30,30)
+    rect.height = 40
+    rect.width = 50
+    println("${rect.height}, ${rect.width}")
+}
+class Rectangle ( val shape:String, var height:Int, var width:Int)
+
+fun main() {
+    val rect = Rectangle("rectangle",30,30)
+    rect.height = 40 //java에서는 setHeight()를 직접 정의해야 하지만 코틀린에서는 자동으로 생성해준다.
+    rect.width = 50
+    println("${rect.height}, ${rect.width}")//get도 마찬가지로 직접 정의하지 않아도 된다.
+}
+
+class Person {
+    var name:String = ""
+    var age:Int = -1
+    var isMarried:Boolean = false
+}
+
+fun main() {
+    val park = Person()
+    park.apply { //with(park{ } 과 똑같은 기능
+        park.name = "Park"
+        park.age = 27
+        park.isMarried = true
+    }
+    with(park){
+        println("name = $name, age=$age")
+    }
+
+    val kang = Person()
+    with(kang){
+        name = "Kang" //인스탄스. 을 생략하여 초기화 할 수 있다.
+        age = 22
+        isMarried = false
+    }
+}
+
