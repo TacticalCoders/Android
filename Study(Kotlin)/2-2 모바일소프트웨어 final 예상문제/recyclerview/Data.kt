30c27c037419b6bcf44c0d3eb56868637f6dc473
littlebuffett <docdoca@naver.com>
2022-01-19 21:25:46 +0900

    merged retrofit-study

---
+++ Study(Kotlin)/2-2 모바일소프트웨어 final 예상문제/recyclerview/Data.kt
@@ -0,0 +1,3 @@
+package com.example.finallastpractice
+
+data class Data(val name:String, val msg:String, val profile:Int)
--- final 예상/recyclerview/Data.kt
+++
@@ -1,3 +0,0 @@
-package com.example.finallastpractice
-
-data class Data(val name:String, val msg:String, val profile:Int)
